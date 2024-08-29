3d74f822b5737dc1823b67a52840a88376b21457
ArhaanAli04 <arhaan.ali2004@gmail.com>
2024-08-29 15:50:03 +0530

    nfc

--- nav-att-assign-react/src/Components/HomePage.jsx
+++ nav-att-assign-react/src/Components/HomePage.jsx
@@ -1,12 +1,13 @@
 // src/Components/Attendance.jsx
 import React from 'react';
 import './HomePage.css'
+
 function HomePage() {
   return (
     <div className='home-page'>
       <h1>Attendance and Assignment Submission</h1>
+      <button>Sign Up Here</button>
       
-      <p>This is the Home page.</p>
     </div>
   );
 }
---
+++ nav-att-assign-react/src/Components/LoginForm.jsx
@@ -0,0 +1,36 @@
+// LoginForm.jsx
+
+import React from 'react';
+import { FaUser, FaLock } from 'react-icons/fa';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import './LoginForm.css';
+
+function LoginForm() {
+  return (
+    <div className="wrapper">
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder="username" required />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="password" required />
+          <FaLock className="icon" />
+        </div>
+        <div className="remember-forgot">
+          <label className="check"><input type="checkbox" /> Remember me</label>
+          <a href="#">Forgot Password?</a>
+        </div>
+        <button type="submit">Login</button>
+        <div className="register-link">
+          <p>
+            Don't have an account? <Link to="/register">Register here</Link> {/* Use Link to navigate */}
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
